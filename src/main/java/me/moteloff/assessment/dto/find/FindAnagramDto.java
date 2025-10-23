6ed0edbe82a8285832847606e4d68d81578b907b
moteloff <moteloffz@gmail.com>
2025-10-24 01:04:57 +0200

    validation

--- src/main/java/me/moteloff/assessment/dto/find/FindAnagramDto.java
+++ src/main/java/me/moteloff/assessment/dto/find/FindAnagramDto.java
@@ -1,6 +1,8 @@
 package me.moteloff.assessment.dto.find;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record FindAnagramDto(
-        String word
+        @NotBlank String word
 ) {
 }
